fa2f2cd97e9ccf5798468a28b05f2791a5d922e1
Wookashi <lukasz.hr@outlook.com>
2025-08-24 22:49:47 +0200

    New languages support.

--- Wookashi.ExtraText/Normalize/Enums/Language.cs
+++ Wookashi.ExtraText/Normalize/Enums/Language.cs
@@ -6,12 +6,11 @@ namespace Wookashi.ExtraText.Normalize.Enums
         Polish = 1,
         German = 2,
         French = 3,
-        // Spanish = 4,
-        // Arabic = 5,
-        // Bengali = 6,
+        Spanish = 4,
+        Swedish = 5,
+        Slovak = 6,
         Czech = 7,
-        // Hungarian = 8,
-        // Serbian = 9,
-        // Slovak = 10
+        Hungarian = 8,
+        Serbian = 9
     }
 }
