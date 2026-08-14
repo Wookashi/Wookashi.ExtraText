d050b1a235957491c8a82a0166245ad2dd94b165
Wookashi <lukasz.hr@outlook.com>
2026-08-14 22:10:41 +0200

    French and Czech language added.

--- Wookashi.ExtraText/Normalize/Enums/Language.cs
+++ Wookashi.ExtraText/Normalize/Enums/Language.cs
@@ -3,7 +3,15 @@ namespace Wookashi.ExtraText.Normalize.Enums
 {
     public enum Language
     {
-        Polish = 2,
-        German = 3
+        Polish = 1,
+        German = 2,
+        French = 3,
+        // Spanish = 4,
+        // Arabic = 5,
+        // Bengali = 6,
+        Czech = 7,
+        // Hungarian = 8,
+        // Serbian = 9,
+        // Slovak = 10
     }
 }
