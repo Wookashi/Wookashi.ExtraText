7949dca37f900e86b70b0ebcbc10229973ab5a12
Wookashi <lukasz.hr@outlook.com>
2026-08-14 22:10:35 +0200

    Hide internals and clean unused code.

--- Wookashi.ExtraText/Normalize/Implementation/LanguageNormalizer.cs
+++ Wookashi.ExtraText/Normalize/Implementation/LanguageNormalizer.cs
@@ -5,10 +5,10 @@ using Wookashi.ExtraText.Normalize.Models;
 
 namespace Wookashi.ExtraText.Normalize.Implementation
 {
-    public class LanguageNormalizer
+    internal sealed class LanguageNormalizer
     {
 
-        public string ReplaceDiacriticalMarks(string text)
+        internal string ReplaceDiacriticalMarks(string text)
         {
             var builder = new StringBuilder(text);
             foreach (var dMark in LanguageDiacriticalMark.Marks)
@@ -18,20 +18,10 @@ namespace Wookashi.ExtraText.Normalize.Implementation
             return builder.ToString();
         }
 
-        public string ReplaceDiacriticalMarks(string text, Language language)
+        internal string ReplaceDiacriticalMarks(string text, Language language)
         {
             var builder = new StringBuilder(text);
             foreach (var dMark in LanguageDiacriticalMark.Marks.Where(x => x.Language == language))
-            {
-                builder.Replace(dMark.Source, dMark.Target);
-            }
-            return builder.ToString();
-        }
-
-        public string ReplaceDiacriticalMarks(string text, Language[] languages)
-        {
-            var builder = new StringBuilder(text);
-            foreach (var dMark in LanguageDiacriticalMark.Marks.Where(x => languages.Contains(x.Language)))
             {
                 builder.Replace(dMark.Source, dMark.Target);
             }
